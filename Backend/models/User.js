1cfe5544d118fa1918daa8aa3f3a3fc462ec5edd
Pragyagit-07 <pragyamakadariya7@gmail.com>
2025-12-30 14:10:23 +0530

    add changes in user.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,16 +1,3 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../db');
-
-// const User = sequelize.define('User', {
-//   id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-//   name: { type: DataTypes.STRING, allowNull: false },
-//   email: { type: DataTypes.STRING, allowNull: false, unique: true },
-//   passwordHash: { type: DataTypes.STRING, allowNull: false },
-//   role: { type: DataTypes.ENUM('admin', 'member'), defaultValue: 'member' }
-// }, { tableName: 'users', timestamps: true });
-
-// module.exports = User;
-
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
 
