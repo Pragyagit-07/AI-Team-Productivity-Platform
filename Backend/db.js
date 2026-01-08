56714ca4fb2ea08454e49dcffbeb1e2a11b9c383
Pragyagit-07 <pragyamakadariya7@gmail.com>
2026-01-08 12:56:02 +0530

    fixed db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,42 +1,42 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT || 'mysql',
-//     logging: false, 
-//   }
-// );
-
-// // connection
-// sequelize.authenticate()
-//   .then(() => console.log(' MySQL Connected'))
-//   .catch(err => console.error('MySQL Connection Error:', err));
-
-// module.exports = sequelize;
-
-
-
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,   
-  process.env.MYSQLUSER,     
-  process.env.MYSQLPASSWORD,   
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    dialect: 'mysql',
-    logging: false,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false, 
   }
 );
 
+// connection
+sequelize.authenticate()
+  .then(() => console.log(' MySQL Connected'))
+  .catch(err => console.error('MySQL Connection Error:', err));
+
 module.exports = sequelize;
+
+
+
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQLDATABASE,   
+//   process.env.MYSQLUSER,     
+//   process.env.MYSQLPASSWORD,   
+//   {
+//     host: process.env.MYSQLHOST,
+//     port: process.env.MYSQLPORT,
+//     dialect: 'mysql',
+//     logging: false,
+//   }
+// );
+
+// module.exports = sequelize;
 
 
 
