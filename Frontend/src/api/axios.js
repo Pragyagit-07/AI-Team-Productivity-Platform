6fe332143254bf4c0215f0550e9c259b575a8daf
Pragyagit-07 <pragyamakadariya7@gmail.com>
2026-01-29 20:02:41 +0530

    add new axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,18 +1,40 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//    baseURL: "http://localhost:5000/api",
+  
+// });
+
+// API.interceptors.request.use((req) => {
+//   // priority: admin token first
+//   const adminToken = localStorage.getItem("adminToken");
+//   const memberToken = localStorage.getItem("memberToken");
+//   const token = adminToken || memberToken;
+//     if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,
 });
 
 API.interceptors.request.use((req) => {
-  // priority: admin token first
   const adminToken = localStorage.getItem("adminToken");
   const memberToken = localStorage.getItem("memberToken");
   const token = adminToken || memberToken;
-    if (token) {
+
+  if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
